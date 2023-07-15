e7978e5c877b62cfb35b0fc26a53f9ab70ec4056
fatemeheydari7430 <fateme.heydari.7430@gmail.com>
2023-07-15 17:07:53 +0330

    first commit

---
+++ products.js
@@ -0,0 +1,39 @@
+export const productsData = [
+
+    { id: 1,
+      title: "queen panel bed" ,
+      price: 10.99,
+      imageUrl: "./img/1.jpg",
+    },
+
+    { id: 2,
+        title: "fateme panel bed" ,
+        price: 12.39,
+        imageUrl: "./img/2.jpg",
+      },
+
+      { id: 3,
+        title: "ali panel bed" ,
+        price: 45.86,
+        imageUrl: "./img/3.jpg",
+      },
+
+      { id: 4,
+        title: "amir panel bed" ,
+        price: 36.11,
+        imageUrl: "./img/4.jpg",
+      },
+
+      { id: 5,
+        title: "hosein panel bed" ,
+        price: 78.31,
+        imageUrl: "./img/5.jpg",
+      },
+
+      { id: 6,
+        title: "maryam panel bed" ,
+        price: 62.84,
+        imageUrl: "./img/6.jpg",
+      },
+      
+]
